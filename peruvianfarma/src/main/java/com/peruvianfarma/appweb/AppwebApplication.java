895365ac4265ec3e231eed987771948eadf0834f
Ruben <rportova.work@gmail.com>
2021-05-02 21:24:47 -0500

    CAMBIOS

--- peruvianfarma/src/main/java/com/peruvianfarma/appweb/AppwebApplication.java
+++ peruvianfarma/src/main/java/com/peruvianfarma/appweb/AppwebApplication.java
@@ -2,9 +2,20 @@ package com.peruvianfarma.appweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AppwebApplication {
+
+
+
+@GetMapping(value="/hello")
+public String getMethodName(){
+	return "administrador/admindex.html";
+}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppwebApplication.class, args);
