be9c34b8054fb1b37f4dbe208f47d86e068d8289
unknown <u17204925@utp.edu.pe>
2021-05-03 19:41:38 -0500

    CAMBIOS Diana

---
+++ peruvianfarma/src/main/java/com/peruvianfarma/appweb/model/Carrito.java
@@ -0,0 +1,11 @@
+package com.peruvianfarma.appweb.model;
+
+public class Carrito {
+
+    private Long id;
+    private String numpedi;
+    private String numtel;
+    private String dni;
+    
+    
+}
